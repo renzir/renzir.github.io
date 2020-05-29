b55ece90f75e3a4eb2476f1f4a5e18ecddccc823
renzir <45017853+renzir@users.noreply.github.com>
2020-05-29 16:19:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function open() {
+  
+
+  document.getElementById("btn-menu").style.width = "100%";
+  document.getElementById("btn-menu").style.display = "block";
+  
+}
+
+function close() {
+  document.getElementById("btn-menu").style.display = "none";
+}
